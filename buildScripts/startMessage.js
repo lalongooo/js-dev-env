02ee53b0457ea270f3935001f6e2d91083b8468b
Jorge <hdez.jeduardo@gmail.com>
2017-12-12 00:22:49 -0600

    Add .babelrc config, use some ES6 syntax

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,4 @@
-var chalk = require('chalk');
+// var chalk = require('chalk'); // This is ES5 syntax
+import chalk from 'chalk';    // This is ES6 syntax. With babel, this should transpile to ES5.
 
 console.log(chalk.green('Starting app in dev mode...'));
